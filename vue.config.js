812819b95e6e5186db0f6daf7a5494589b8c2833
manhd89 <150607244+manhd89@users.noreply.github.com>
2026-01-22 07:12:16 +0700

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': require('path').resolve(__dirname, 'src')
+      }
+    }
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true
+  }
+})
